39508f75d0076a537c407359de02dbe301f47d28
charlie <ljwcharlie@gmail.com>
2023-03-08 09:53:19 +0800

    initial fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}?clientID=lljXuqzLdmQYMCujc5ctORAcHZoI0x6HT5_jxrlso_Q`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return <h2>stock photos starter</h2>;
 }
 
-export default App
+export default App;
